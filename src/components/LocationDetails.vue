326a02601bfcc17a7ef2d49ebfbb70733c358d25
Josef Donlin <jdonlin@gmail.com>
2024-06-26 20:20:09 -0700

    wrangle LocationDetails elements into flexboxes
    There are many lessons to be learned in CSS.

--- src/components/LocationDetails.vue
+++ src/components/LocationDetails.vue
@@ -1,16 +1,24 @@
 <template>
-  <div v-if="currentLocation">
-    <img :src="currentLocation.banner_img" />
-    <img :src="currentLocation.logo_img" />
-    <p>Name: {{ currentLocation.name }}</p>
-    <p>Description: {{ currentLocation.description }}</p>
-    <p>Hours: {{ currentLocation.hours }}</p>
-    <p>
-      Address: {{ currentLocation.address1 }} {{ currentLocation.address2 }}
-      {{ currentLocation.city }}, {{ currentLocation.state }}
-    </p>
+  <div v-if="currentLocation" class="location-details">
+    <div class="top-box">
+      <img class="location-banner" :src="currentLocation.banner_img" />
+    </div>
+    <div class="middle-boxes">
+      <div class="middle-left">
+        <img class="location-logo" :src="currentLocation.logo_img" />
+      </div>
+      <div class="middle-right">Welcome to {{ currentLocation.name }}</div>
+    </div>
+    <div class="bottom-box">
+      <p>{{ currentLocation.description }}</p>
+      <p>Hours: {{ currentLocation.hours }}</p>
+      <p>
+        Address: {{ currentLocation.address1 }} {{ currentLocation.address2 }}
+        {{ currentLocation.city }}, {{ currentLocation.state }}
+      </p>
+    </div>
   </div>
-  <div v-else>Welcome!</div>
+  <div v-else>Welcome! Please select a location on the left nav-bar to get started.</div>
 </template>
 
 <script setup>
@@ -20,3 +28,42 @@ const store = useStore()
 
 const currentLocation = computed(() => store.getters.currentLocation)
 </script>
+
+<style scoped>
+.location-details {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+.top-box {
+  flex: 20%;
+  max-height: 250px;
+  align-items: center;
+  justify-content: center;
+}
+.middle-boxes {
+  display: flex;
+}
+.middle-left {
+  flex: 50%;
+  max-height: 150px;
+}
+.middle-right {
+  display: flex;
+  flex: 50%;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+.bottom-box {
+  flex: 20%;
+}
+.location-banner {
+  max-height: 100%;
+  width: 100%;
+}
+.location-logo {
+  max-height: 100%;
+  width: 100%;
+}
+</style>
